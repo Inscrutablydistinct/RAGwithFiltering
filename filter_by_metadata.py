5863754ef219631f1f7a4ca471b12fc94c9f6007
Inscrutablydistinct <121133033+Inscrutablydistinct@users.noreply.github.com>
2024-07-01 13:26:40 +0530

    Update filter_by_metadata.py

--- filter_by_metadata.py
+++ filter_by_metadata.py
@@ -25,33 +25,32 @@ def filter_attributes(metadata_entry, key, value):
     if (key=='title'):
         cos_sim = compute_cosine_similarity(metadata_entry['title'], value)
         return cos_sim*10
-    else if (key == 'author'):
+    elif (key == 'author'):
         return 1.0 if value in metadata_entry['author'] else 0.0
-    else if (key == 'abstract'):
+    elif (key == 'abstract'):
         cos_sim = compute_cosine_similarity(metadata_entry['abstract'], value)
         return cos_sim*10
-    else if (key == 'keywords'):
+    elif (key == 'keywords'):
         cos_sim = compute_cosine_similarity(metadata_entry['abstract'], value)
         return cos_sim*10
-    else if (key == 'publication_date'):
+    elif (key == 'publication_date'):
         op = value[0] if value[1].isdigit() else value[0:2]
         value = value[len(op):]
         filter_date = datetime.strptime(value, "%Y-%m-%d")
         if metadata_entry['publication_date'] == "N/A":
             return 0.0
         entry_date = datetime.strptime(metadata_entry['publication_date'], "%Y-%m-%d")
-        match op:
-            case '>':
-                return 2.0 if entry_date > filter_date else -6.0
-            case '>=':
-                return 2.0 if entry_date >= filter_date else -6.0
-            case '<':
-                return 2.0 if entry_date < filter_date else -6.0
-            case '<=':
-                return 2.0 if entry_date <= filter_date else -6.0
-            case _:
-                return 2.0 if entry_date == filter_date else -6.0
-    else if (key == 'results'):
+        if (op == '>'):
+            return 2.0 if entry_date > filter_date else -6.0
+        elif (op == '>='):
+            return 2.0 if entry_date >= filter_date else -6.0
+        elif (op == '<'):
+            return 2.0 if entry_date < filter_date else -6.0
+        elif (op == '<='):
+            return 2.0 if entry_date <= filter_date else -6.0
+        else:
+            return 2.0 if entry_date == filter_date else -6.0
+    elif (key == 'results'):
         if (type(metadata_entry['results'])==list):
             metadata_entry['results'] = " ".join(metadata_entry['results'])
         cos_sim = compute_cosine_similarity(metadata_entry['results'], value)
